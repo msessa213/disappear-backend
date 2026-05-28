97577ff024e330c6c3f35cf0fb656508081c959c
Disappear DevOps <devops@disappear.local>
2026-05-28 13:04:11 -0400

    Update render-build to nuke existing venv

--- render-build.sh
+++ render-build.sh
@@ -2,6 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Force remove the broken virtual environment
+rm -rf /opt/render/project/src/.venv
+
 # Install dependencies using the system python
 pip install --upgrade pip
 pip install -r backend/requirements.txt
