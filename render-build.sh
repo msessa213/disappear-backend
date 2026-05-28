ccb1ad2b99a7e29eb933e57146daf078918fe30e
Disappear DevOps <devops@disappear.local>
2026-05-28 19:55:19 -0400

    Final deletion of render-build.sh

--- render-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-# Upgrade pip and install with the flag that bypasses the PEP 668 restriction
-pip install --upgrade pip --break-system-packages
-pip install -r backend/requirements.txt --break-system-packages
